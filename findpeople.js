17f63955f229a791957c49b088bedb095f3be35c
ephoenix36 <56854148+ephoenix36@users.noreply.github.com>
2021-01-23 18:54:28 -0500

    Update findpeople.js

--- findpeople.js
+++ findpeople.js
@@ -1,7 +1,12 @@
+function addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
 function displayUnfulfilled() {
   var ordersRef = db.collection("orders");
-
-//   docRef.orderBy("time");
 
   ordersRef.get().then(function(querySnapshot) {
     var container = document.getElementById("container");
@@ -20,33 +25,12 @@ function displayUnfulfilled() {
 
         var label = document.createElement("Label");
         label.setAttribute("for", userData["user"]);
-        var date = new Date(userData["time"]["seconds"]);
-        label.innerHTML = "Location: " + userData["location"] + " | Time: " + date.getFullYear();
-        // label.setAttribute("innerHTML", "Location: ", userData["location"], ", Time: ", userData["time"]);
+        var date = new Date(userData["time"]["seconds"]*1000);
+        label.innerHTML = " " + userData["location"] + " | " + addZero(date.getHours() % 12) + ":" + addZero(date.getMinutes());
 
         container.append(input);
         container.append(label);
     })
-
-    // if (doc.exists) {
-    //     console.log("Document data:", doc.data());
-
-    //     var myDataMap = doc.data();
-    //     times = myDataMap["times"];
-    //     var i;
-    //     for (i = 0; i < times.length; i++) {
-    //         var list = document.getElementById('order list');
-
-    //         var node = document.createElement("li");
-    //         var textnode = document.createTextNode(times[i]);
-    //         node.appendChild(textnode);
-    //         list.appendChild(node);
-    //     }
-
-    // } else {
-    //     // doc.data() will be undefined in this case
-    //     console.log("No such document!");
-    // }
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });
