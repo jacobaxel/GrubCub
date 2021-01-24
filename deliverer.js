546ae118f60b90b33e4870498fb610ab6c2de8ec
ephoenix36 <56854148+ephoenix36@users.noreply.github.com>
2021-01-24 01:48:52 -0500

    Update deliverer.js

--- deliverer.js
+++ deliverer.js
@@ -1,10 +1,3 @@
-function addZero(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-
 function displayUnfulfilled() {
 
   var ordersRef = db.collection("orders");
@@ -15,36 +8,27 @@ function displayUnfulfilled() {
     var counter = 0;
 
     querySnapshot.forEach(function(doc) {
-        counter++;
 
+        // Limits the number of orders displayed to 20
+        counter++;
         if (counter > 20) {
             return false;
         }
 
         var userData = doc.data();
 
-        console.log(userData["time"]);
-        console.log(container);
-
         var input = document.createElement("INPUT");
         input.setAttribute("type", "checkbox");
         input.setAttribute("id", userData["order_number"]);
-        input.setAttribute("name", "name");
         input.setAttribute("value", userData["pickup_time"]);
 
         var label = document.createElement("Label");
-        label.setAttribute("for", userData["user"]);
-        // var date = new Date(userData["time"]["seconds"]*1000);
-        label.innerHTML = userData["pickup_time"] + " | Pickup From: " + userData["dining_hall"] + " | Deliver To: " + userData["dorm"]
-        // " " + addZero(date.getHours() % 12) + ":" + addZero(date.getMinutes()) + " | Deliver To: " + userData["location"];
+        label.setAttribute("for", userData["order_number"]);
+        label.innerHTML = userData["pickup_time"] + " | Pickup From: " + userData["dining_hall"] + " | Deliver To: " + userData["dorm"];
 
         container.append(input);
         container.append(label);
 
-        // var par = document.createElement("p");
-        // par
-        // var textnode = document.createTextNode('<br/>');
-        // container.append(textnode);
         $('#checklist').append("<br>");
         
     })
@@ -54,21 +38,54 @@ function displayUnfulfilled() {
 }
 
 var btn = document.getElementById("submit");
-
-if(btn) {
-  btn.addEventListener("click", confirmation);
-}
+if (btn) { btn.addEventListener("click", confirmation); }
 
 function confirmation() {
-  console.log("confirmation clicked");
+
+  var ordersRef = db.collection("orders");
 
   let checked = $("#checklist input[type=checkbox]:checked");
+  var checked_order_numbers = [];
 
-  // checked.forEach(function(order) {
-  //   console.log(order);
-  // });
+  var i;
+  for(i = 0; i < checked.length; i++) {
+    checked_order_numbers[i] = checked[i].id;
+  }
 
-  console.log(checked);
+  console.log(checked_order_numbers);
+
+  ordersRef.get().then(function(querySnapshot) {
+    var container = document.getElementById("answers");
+    var counter = 1;
+
+    querySnapshot.forEach(function(doc) {
+
+        // console.log(doc);
+
+        var userData = doc.data();
+
+        if (checked_order_numbers.indexOf(userData["order_number"]) > -1) {
+          ordersRef.doc(userData["order_number"]).delete().then(function() {
+            var tag = document.createElement("p");
+            var text = document.createTextNode(counter + ": " + userData["pickup_time"] + " | Pickup From: " + userData["dining_hall"] + " | Deliver To: " + userData["dorm"] + " | Order Number: " + userData["order_number"] + " | Phone Number: " + userData["phone_number"]);
+            tag.appendChild(text);
+            container.appendChild(tag);
+
+            counter++;
+            console.log("Document Successfully Deleted!");
+
+          }).catch(function(error) {
+              console.error("Error removing document: ", error);
+          });
+        }
+
+        // change picked up field to true;
+        // print message
+        
+    })
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
 
   return false;
 }
